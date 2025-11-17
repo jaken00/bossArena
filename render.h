715b76038e2a62638f03eeecbcef1b9c1eaabec6
Jacob Nunes <139832504+jaken00@users.noreply.github.com>
2025-11-16 22:37:51 -0800

    render setup

--- render.h
+++ render.h
@@ -1,4 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-
+//write these out tommorow
+//fix waka time key
+void renderEntities(SDL_Renderer *renderer, SDL_Rect *player, SDL_Rect *enemy);
+void renderProjectiles();
