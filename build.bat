c31ec4b001dd5e834f5cf54311ce56a3c9b6e071
Jacob Nunes <e88993@revelyst.com>
2025-11-18 07:55:34 -0800

    file structure organization

--- build.bat
+++ build.bat
@@ -1,9 +1,11 @@
 @echo off
-gcc main.c game.c ui.c render.c -o game.exe ^
+gcc src/*.c -Iinclude ^
     -IC:/msys64/mingw64/include/SDL2 ^
     -LC:/msys64/mingw64/lib ^
     -lmingw32 -lSDL2main -lSDL2 -lSDL2_image ^
-    -Wall 
+    -o game.exe ^
+    -Wall
+ 
 if %errorlevel% equ 0 (
     echo Build successful!
     game.exe
